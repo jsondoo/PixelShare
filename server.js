f4957d0bf333cc5886fe035f529b246b350c7715
Harsh Patel <harsh700ca@gmail.com>
2018-01-13 23:33:23 -0800

    server state initialize

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ var server = app.listen(process.env.PORT || 3000);
 app.use(express.static('public'));
 var io = require('socket.io')(server);
 
+var grid = create_grid()
+io.sockets.on('connection', new_connection);
 
-io.sockets.on('connection', newConnection);
-
-function newConnection(socket) {
-    socket.emit('state', gridPixels);
+function new_connection(socket) {
+    socket.emit('state', grid);
 }
 
-
-
-
+function create_grid() {
+    for(i = 0; i < 300; i++){
+        grid[i] = new Array(300).fill({
+            'r': 255,
+            'g': 255,
+            'b': 255
+        })
+    }
+}
