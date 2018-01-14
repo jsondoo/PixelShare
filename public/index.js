140bfbcdaba5c8412fa76e789d4bea21c7acc7e9
jsondoo <jsondoo@users.noreply.github.com>
2018-01-14 11:17:02 -0800

    hypothesizign

--- public/index.js
+++ public/index.js
@@ -28,7 +28,30 @@ function setup() {
 }
 
 function update_timer(wait_time) {
+    // set initial time
+    var distance = wait_time
+    var minutes = Math.floor(distance / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("time").innerHTML = minutes + "m " + seconds + "s ";
     
+    // update timer every second
+    var x = setInterval(function() {
+        // grab current time remaining
+        var distance = wait_time
+
+
+        var minutes = Math.floor(distance / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("time").innerHTML = minutes + "m " + seconds + "s ";
+        console.log('called')
+        distance -= 1;
+        
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("time").innerHTML = "Ready to click";
+        }
+    }, 1000);
 }
 
 function update_pixel(update) {
