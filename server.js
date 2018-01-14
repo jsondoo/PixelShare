15e2b6b3b63d5c0f14214ff4206505e073c10ed9
Harsh Patel <harsh700ca@gmail.com>
2018-01-13 23:57:20 -0800

    server functions completed

--- server.js
+++ server.js
@@ -9,10 +9,23 @@ var grid = create_grid()
 io.sockets.on('connection', new_connection);
 
 function new_connection(socket) {
+    console.log('asdf')
     socket.emit('state', grid);
+
+    socket.on('send_pixel_update', update_pixel);
 }
 
+function update_pixel(pixel) {
+    x = pixel.x
+    y = pixel.y
+    grid[x][y] = {
+        'r': pixel.r,
+        'g': pixel.g,
+        'b': pixel.b
+    }
+}
 function create_grid() {
+    var grid = []
     for(i = 0; i < 300; i++){
         grid[i] = new Array(300).fill({
             'r': 255,
